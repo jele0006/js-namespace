25c867f0d94d42b21adffd0a908281686384bf78
Wraithshroud <jele0006@algonquinlive.com>
2017-11-27 21:29:31 -0500

    js.namespace asn
    js assignment

---
+++ namespace.js
@@ -0,0 +1,20 @@
+var JELE0006 = {
+    prop1: 123,
+    init: function(){
+        //find your div
+        //add your event listeners obj.addEvenListener("click", JELE0006.jjj);
+        JELE0006.jjj();
+    },
+    prop2: 456,
+    jjj: function(){
+        
+    }
+}
+
+
+//https://prof3ssorst3v3.github.io/js-namespace/index.html
+//professorSt3v3.github.io/js-namespace/index.html?username=jele0006     (to load your script only)
+//https://prof3ssorst3v3.github.io/js-namespace/index.html?user=jele0006
+
+
+//not sure which one works just yet
